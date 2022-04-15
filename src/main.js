77b8ab26abaacd59a8b56dac28ae12ff3bc41b92
David Nguyen <nguy3482@umn.edu>
2022-04-15 15:36:56 -0500

    add basic ckeditor

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import { auth } from "@/firebaseConfig";
 import './index.css'
+import CKEditor from '@ckeditor/ckeditor5-vue';
+
 let app;
 
 auth.onAuthStateChanged(() => {
   if (!app) {
-    app = createApp(App).use(router).mount('#app')
+    app = createApp(App).use(router).use(CKEditor).mount('#app')
   }
 })
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page {{editorData}}</h1>
+     <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
   </div>
 </template>
+
+<script>
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+    export default {
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>',
+                editorConfig: {
+                    // The configuration of the editor.
+                }
+            };
+        }
+    }
+</script>
