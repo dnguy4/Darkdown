e73a86ed4fb30ed7c02f398988183b2596502bd0
David Nguyen <nguy3482@umn.edu>
2022-04-17 21:39:04 -0500

    add title to texteditor and add routing (commented out rn)

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,30 +1,27 @@
 <template>
-  <div class="border-4 border-gray-600 h-screen overflow-auto">
-    <!-- <div class="flex">
-    <div class="hidden md:block md:basis-1/4 bg-gradient-to-r from-cyan-500 to-slate-50"></div>
-    <div class="grow border-4 border-gray-50"> -->
+  <div class="border-4 border-gray-600 h-screen overflow-hidden">
+    <editable-header @edited="updateTitle"/>
     <ckeditor class="h-screen" 
         :editor="editor" 
         v-model="editorData" 
         :config="editorConfig"  
         @ready="onReady"></ckeditor>
-        <!-- </div>
-    </div> -->
   </div>
 </template>
 
 <script>
     import Editor from 'ckeditor5-custom-build'
+    import EditableHeader from './EditableHeader.vue';
 
-    // Editor.builtinPlugins.map( plugin => console.log(plugin.pluginName) );
+    Editor.builtinPlugins.map( plugin => console.log(plugin.pluginName) );
 
     export default {
+        components: { EditableHeader },
         data() {
             return {
                 editor: Editor,
                 editorData: '<p>Content of the editor.</p>',
                 editorConfig: {
-                    // The configuration of the editor
                     autosave: {
                         waitingTime: 3000, //in ms
                         save( editor ) {
@@ -41,6 +38,9 @@
                     editor.ui.view.toolbar.element,
                     editor.ui.getEditableElement()
                 );
+            },
+            updateTitle: function(newTitle) {
+                console.log(newTitle);
             }
         }
     }
