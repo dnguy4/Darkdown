846b3dcd240fe865c142c8fe296e342cbc4de5f3
Ace <akarthut99@gmail.com>
2022-04-19 02:57:10 -0500

    home page and login button update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotFoundView from '../views/NotFoundView'
+import { auth } from "@/firebaseConfig.js";
 
 const routes = [
   {
@@ -11,6 +12,9 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    meta: {
+      requiresAuth: true,
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -26,6 +30,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
+  if (requiresAuth && !auth.currentUser) {
+    next("/");
+  } else {
+    next();
+  }
+});
 
 export default router
