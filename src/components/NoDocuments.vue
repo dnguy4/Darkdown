8d5e40b1f2b299bdd7f13cd790dfa367a02dc464
Julia Pan <pan00079@umn.edu>
2022-04-25 15:15:07 -0500

    restructuring main page, adding and styling no-document and 404 pages

---
+++ src/components/NoDocuments.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-1/2">
+      <div class="m-10 text-5xl">
+        You currently don't have any documents.
+      </div>
+      <button class="large-button">
+            Create a new document?
+      </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
